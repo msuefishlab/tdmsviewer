97e4143f3d2dd179187c3a7f5c11edf3d3a6c588
Colin <colin.diesh@gmail.com>
2017-01-04 16:17:35 -0500

    Add lintr

--- server.R
+++ server.R
@@ -8,8 +8,8 @@ flog.threshold(DEBUG)
 
 # open file
 shinyServer(function(input, output) {
-  
- 
+
+
     dataInput <- reactive({
         withProgress(message = 'Loading...', value = 0, {
             my_file = file(paste0(input$dir, '/', input$dataset), "rb")
@@ -20,115 +20,115 @@ shinyServer(function(input, output) {
         })
     })
 
-    
+
     output$dirs <- renderUI({
         dirs = list.files('data', full.names = T)
         selectInput("dir", "Data group", c("Choose directory", dirs))
     })
-  
-  
+
+
     output$datasets <- renderUI({
-        if(is.null(input$dir)) {
+        if (is.null(input$dir)) {
             return()
         }
-        if(input$dir == "Choose directory") {
+        if (input$dir == "Choose directory") {
             return()
         }
         tdmss = list.files(input$dir, pattern = ".tdms$")
         selectInput("dataset", "TDMS File", c("Choose tdms", tdmss))
     })
-   
+
     output$objects <- renderUI({
-        if(is.null(input$dataset)) {
+        if (is.null(input$dataset)) {
             return()
         }
-        if(input$dataset == "Choose tdms") {
+        if (input$dataset == "Choose tdms") {
             return()
         }
         datatable <- dataInput()
         selectInput("object", "TDMS Object", c("Choose object", ls(datatable$objects)))
     })
-  
-  
+
+
     output$startTimer <- renderUI({
-        if(is.null(input$object)) {
+        if (is.null(input$object)) {
             return()
         }
-        if(input$object == "Choose object") {
+        if (input$object == "Choose object") {
             return()
         }
 
-        datatable<-dataInput()
+        datatable <- dataInput()
         r = datatable$objects[[input$object]]
         t = r$time_track()
         print(min(t))
         print(max(t))
 
-        sliderInput("startTime", "Start", min=floor(min(t)), max=ceil(max(t)), value = 0)
+        sliderInput("startTime", "Start", min = floor(min(t)), max = ceiling(max(t)), value = 0)
     })
-  
-  
-    output$endTimer <- renderUI({
-        if(is.null(input$object)) {
-            return()
-        }
-        if(input$object == "Choose object") {
-            return()
-        }
-        
 
-        datatable<-dataInput()
+
+    output$endTimer <- renderUI({
+        if (is.null(input$object)) {
+            return()
+        }
+        if (input$object == "Choose object") {
+            return()
+        }
+
+
+        datatable <- dataInput()
         r = datatable$objects[[input$object]]
         t = r$time_track()
-        sliderInput("endTime", "End", min=min(t), max=max(t), value = 0.5)
+        sliderInput("endTime", "End", min = min(t), max = max(t), value = 0.5)
     })
-    
-    
+
+
     output$fineStartTimer <- renderUI({
-        if(is.null(input$endTime)) {
+        if (is.null(input$endTime)) {
             return()
         }
 
-        sliderInput("fineStartTime", "Adjust start", min=input$startTime, max=input$endTime, value = input$startTime)
+        sliderInput("fineStartTime", "Adjust start", min = input$startTime, max = input$endTime, value = input$startTime)
     })
-    
-    
+
+
     output$fineEndTimer <- renderUI({
-        if(is.null(input$startTime)) {
+        if (is.null(input$startTime)) {
             return()
         }
-        sliderInput("fineEndTime", "Adjust end", min=input$startTime, max=input$endTime, value = input$endTime)
+        sliderInput("fineEndTime", "Adjust end", min = input$startTime, max = input$endTime, value = input$endTime)
     })
-    
+
     output$superFineStartTimer <- renderUI({
-        if(is.null(input$fineEndTime)) {
+        if (is.null(input$fineEndTime)) {
             return()
         }
-        sliderInput("superFineStartTime", "Adjust start (finetune)", min=input$fineStartTime, max=input$fineEndTime, value = input$fineStartTime)
+        sliderInput("superFineStartTime", "Adjust start (finetune)", min = input$fineStartTime, max = input$fineEndTime, value = input$fineStartTime)
     })
-    
-    
+
+
     output$superFineEndTimer <- renderUI({
-        if(is.null(input$fineStartTime)) {
+        if (is.null(input$fineStartTime)) {
             return()
         }
-        sliderInput("superFineEndTime", "Adjust end (finetune)", min=input$fineStartTime, max=input$fineEndTime, value = input$fineEndTime)
+        sliderInput("superFineEndTime", "Adjust end (finetune)", min = input$fineStartTime, max = input$fineEndTime, value = input$fineEndTime)
     })
-   
-    
-    
-    
+
+
+
+
     output$distPlot <- renderPlot({
-        if(is.null(input$object)) {
+        if (is.null(input$object)) {
             return()
         }
-        if(input$dataset == "Choose object") {
+        if (input$dataset == "Choose object") {
             return()
         }
 
-        datatable<-dataInput()
+        datatable <- dataInput()
 
-        if(is.null(input$superFineStartTime)) {
+        if (is.null(input$superFineStartTime)) {
             return()
         }
 
@@ -138,11 +138,10 @@ shinyServer(function(input, output) {
         r = datatable$objects[[input$object]]
         t = r$time_track()
         s = r$data
-       
-        #values = ret$time > s & ret$time < e
-        plot(t[1:100], s[1:100], type='l', xlab='time', ylab='volts')
+
+        plot(t[1:100], s[1:100], type = 'l', xlab = 'time', ylab = 'volts')
     })
-  
+
 
 
 })
--- ui.R
+++ ui.R
@@ -2,10 +2,10 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
+
   # Application title
   titlePanel("TDMS Viewer - Gallant Lab"),
-  
+
   # Sidebar with a slider input for the number of bins
   sidebarLayout(
     sidebarPanel(
@@ -19,7 +19,7 @@ shinyUI(fluidPage(
       uiOutput("superFineStartTimer"),
       uiOutput("superFineEndTimer")
     ),
-    
+
     # Show a plot of the generated distribution
     mainPanel(
       plotOutput("distPlot")
