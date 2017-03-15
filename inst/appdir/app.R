b3914fbea92dc4d364781365ca50e6c87fe0a4c0
Colin <colin.diesh@gmail.com>
2017-03-15 14:32:53 -0400

    Combine ui/server into app.R

--- inst/appdir/app.R
+++ inst/appdir/app.R
@@ -1,4 +1,40 @@
-shinyServer(function(input, output, session) {
+ui = function() {
+    fluidPage(
+        titlePanel("TDMS Viewer - Gallant Lab"),
+
+        # Sidebar with a slider input for the number of bins
+        sidebarLayout(
+            sidebarPanel(
+                p("Use the slider to extend range of the plot or click-and-drag your mouse over an area of the plot to zoom in"),
+                shinyFiles::shinyDirButton('dir', label = 'Directory select', title = 'Please select a directory'),
+                uiOutput("datasets"),
+                uiOutput("objects"),
+                p("TDMS file properties"),
+                verbatimTextOutput("distProperties"),
+                p("TDMS channel properties"),
+                verbatimTextOutput("distChannel")
+            ),
+
+            # Show a plot of the generated distribution
+            mainPanel(
+                p("Zoom in/Zoom out"),
+                actionButton("zoomIn", label = "+"),
+                actionButton("zoomOut", label = "-"),
+                actionButton("moveLeft", label = "<"),
+                actionButton("moveRight", label = ">"),
+                uiOutput("sliderRange"),
+                plotOutput("distPlot",
+                    brush = brushOpts(
+                        id = "plot_brush",
+                        resetOnNew = T,
+                        direction = "x"
+                    )
+                )
+            )
+        )
+    )
+}
+server = function(input, output, session) {
     shinyFiles::shinyDirChoose(input, 'dir', session = session, roots = c(home = basedir))
 
     dataInput = reactive({
@@ -80,10 +116,7 @@ shinyServer(function(input, output, session) {
     })
 
     output$datasets = renderUI({
-        if (is.null(input$dir)) {
-            return()
-        }
-
+        print(input$dir)
         myDir = do.call(file.path, c(basedir, input$dir$path))
         if (!file.exists(myDir)){
             return()
@@ -177,10 +210,15 @@ shinyServer(function(input, output, session) {
         }
         mytext
     })
+
     observeEvent(input$dir, {
+        print('here')
         session$doBookmark()
     })
+
     onBookmarked(function(url) {
         updateQueryString(url)
     })
-})
+}
+
+shinyApp(ui = ui, server = server, enableBookmarking = "url")
